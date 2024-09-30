0520576c6836df8ce2fa56368464ade303e0b804
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2024-09-30 15:39:37 +0100

    Refactor: Add useQuery to usePlatfroms and useGenres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { CACHE_KEY_GENRES } from "../services/constants";
+import useData, { FetchResponse } from "./useData";
 import genres from "../data/genres";
+import axios from "axios";
+import apiClient from "../services/api-client";
 
 export interface Genre {
   id: number;
@@ -7,6 +12,16 @@ export interface Genre {
 }
 
 // const useGenres = () => useData<Genre>("/genres");
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+// const useGenres = () => ({ data: genres, isLoading: false, error: null });
+
+const useGenres = () => {
+  return useQuery({
+    queryKey: CACHE_KEY_GENRES,
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: genres.length, results: genres },
+  });
+};
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,8 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse } from "./useData";
+import { CACHE_KEY_PLATFORMS } from "../services/constants";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
 export interface Platform {
   id: number;
@@ -6,6 +10,17 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+
+const usePlatforms = () =>
+  useQuery({
+    queryKey: CACHE_KEY_PLATFORMS,
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
